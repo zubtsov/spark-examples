1a248613967c201bee5906e3aaa1c37564f30179
Ruslan_Zubtsov <Ruslan_Zubtsov@epam.com>
2022-02-19 19:41:14 +0300

    refactored, added bucketing test

--- src/test/scala/com.github.zubtsov/SparkFunSuite.scala
+++ src/test/scala/com.github.zubtsov/SparkFunSuite.scala
@@ -1,14 +1,28 @@
 package com.github.zubtsov
 
+import com.github.zubtsov.SparkFunSuite.SparkWarehousePath
 import org.apache.spark.sql.SparkSession
+import org.scalatest.BeforeAndAfterAll
 import org.scalatest.funsuite.AnyFunSuite
 
-class SparkFunSuite extends AnyFunSuite {
+import java.io.File
+import scala.reflect.io.Directory
+
+
+class SparkFunSuite extends AnyFunSuite with BeforeAndAfterAll {
   protected val ss = SparkSession
     .builder()
     .master("local[*]")
-    .config("spark.sql.warehouse.dir", "target/spark-warehouse")
+    .config("spark.sql.warehouse.dir", SparkWarehousePath)
 
   protected val spark = ss
     .getOrCreate()
+
+  override protected def beforeAll(): Unit = {
+    new Directory(new File(SparkWarehousePath)).deleteRecursively()
+  }
+}
+
+object SparkFunSuite {
+  val SparkWarehousePath = "target/spark-warehouse"
 }
--- src/test/scala/com.github.zubtsov/WriteTests.scala
+++ src/test/scala/com.github.zubtsov/WriteTests.scala
@@ -1,6 +1,11 @@
 package com.github.zubtsov
 
+import com.github.zubtsov.SparkFunSuite.SparkWarehousePath
 import org.scalatest.Assertions
+import org.apache.spark.sql.functions._
+
+import java.io.File
+import scala.reflect.io.Directory
 
 class WriteTests extends SparkFunSuite {
   test("Can't use bucketing with save method") {
@@ -9,5 +14,25 @@ class WriteTests extends SparkFunSuite {
       .bucketBy(10, "id")
 
       Assertions.assertThrows[org.apache.spark.sql.AnalysisException](writer.save("target/bucketed_table1"))
+  }
+
+  test("Bucketing controls number of files written in one task (per memory partition)") {
+    val numberOfFilesPerTask = 2 //number of tasks = number of partitions in memory
+    val numberOfFilePartitions = 5
+
+    val writer = spark.range(0, 1000, 1, numberOfFilePartitions)
+      .write.mode("overwrite").format("csv")
+      .bucketBy(numberOfFilesPerTask, "id")
+
+    val tableName = "bucketed_table1"
+    writer.saveAsTable(tableName)
+
+    val groupedByPartition = new Directory(new File(s"$SparkWarehousePath/$tableName"))
+      .list.toSeq
+      .filter(p => p.name.endsWith(".csv"))
+      .groupBy(p => p.name.substring(0, 10))
+
+    Assertions.assertResult(numberOfFilePartitions)(groupedByPartition.size)
+    groupedByPartition.values.foreach(buckets => Assertions.assertResult(numberOfFilesPerTask)(buckets.size))
   }
 }
